24ca373ae9816a861c737817c58cbedfec0ab849
iceledor <danijar.gusev@gmail.com>
2016-10-06 11:27:54 +0300

    .NET Core project

---
+++ src/.NET Core/AdditionalCoreHelper/src/AdditionalCoreHelper/Logging/ILoggingService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalCorehelper.Logging
+{
+    public interface ILoggingService
+    {
+        void Debug(Exception exception);
+        void Debug(string format, params object[] args);
+        void Debug(Exception exception, string format, params object[] args);
+        void Error(Exception exception);
+        void Error(string format, params object[] args);
+        void Error(Exception exception, string format, params object[] args);
+        void Fatal(Exception exception);
+        void Fatal(string format, params object[] args);
+        void Fatal(Exception exception, string format, params object[] args);
+        void Info(Exception exception);
+        void Info(string format, params object[] args);
+        void Info(Exception exception, string format, params object[] args);
+        void Trace(Exception exception);
+        void Trace(string format, params object[] args);
+        void Trace(Exception exception, string format, params object[] args);
+        void Warn(Exception exception);
+        void Warn(string format, params object[] args);
+        void Warn(Exception exception, string format, params object[] args);
+    }
+}
